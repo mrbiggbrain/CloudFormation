e6353d01025d890f13707a6c0b692725b9a8b950
Nick Young <mrbiggbrain@gmail.com>
2023-10-05 14:18:57 -0400

    Removing Code

--- Email Attachment Extractor/code/index.js
+++
@@ -1,7 +0,0 @@
-// Import S3 Client
-const { S3Client, GetObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3");
-
-// Import MailParser
-const simpleParser = require('mailparser').simpleParser;
-
-// Get the 
